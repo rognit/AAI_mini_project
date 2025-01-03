edd16b3aa692b3157ba4fe455151b20355f75948
rognit <pierre.brg314@gmail.com>
2025-01-03 12:29:52 +0100

    avoiding datasets with missing values

--- Dataset.py
+++ Dataset.py
@@ -1,5 +1,4 @@
 from ucimlrepo import fetch_ucirepo
-import pandas as pd
 
 
 class Dataset:
@@ -11,25 +10,25 @@ class Dataset:
     DATASETS_IDS = {
         "wine_quality": 186,
         "predict_students_dropout_and_academic_success": 697,
-        "mushroom": 73,
+        #"mushroom": 73, # missing values :/
         #"air_quality": 360, # 0 targets :/
         "estimation_of_obesity_levels_based_on_eating_habits_and_physical_condition": 544,
         "spambase": 94,
         "seoul_bike_sharing_demand": 560,
         "optical_recognition_of_handwritten_digits": 80,
-        "ai4i_2020_predictive_maintenance_dataset": 601, # 6 features for 6 targets...
+        #"ai4i_2020_predictive_maintenance_dataset": 601, # 6 features for 6 targets...
         #"apartment_for_rent_classified": 555, # 0 targets :/
-        "communities_and_crime": 183,
-        "productivity_prediction_of_garment_employees": 597,
-        "drug_consumption_quantified": 373, # 12 features for 19 targets...
-        "myocardial_infarction_complications": 579,
+        #"communities_and_crime": 183, # missing values :/
+        #"productivity_prediction_of_garment_employees": 597, # missing values :/
+        #"drug_consumption_quantified": 373, # 12 features for 19 targets...
+        #"myocardial_infarction_complications": 579, #missing values :/
         "parkinsons_telemonitoring": 189,
         "aids_clinical_trials_group_study_175": 890,
         "iranian_churn": 563,
         "taiwanese_bankruptcy_prediction": 572,
         "room_occupancy_estimation": 864,
         "infrared_thermography_temperature": 925,
-        "support2": 880,
+        #"support2": 880, # missing values :/
         "solar_flare": 89,
         "image_segmentation": 50,
         #"facebook_live_sellers_in_thailand": 488, # 0 targets :/
@@ -37,9 +36,9 @@ class Dataset:
         "steel_plates_faults": 198,
         #"pedestrians_in_traffic": 536, # 0 targets :/
         "hepatitis_c_virus_hcv_for_egyptian_patients": 503,
-        "communities_and_crime_unnormalized": 211,
+        #"communities_and_crime_unnormalized": 211, # missing values :/ and same as communities_and_crime id 183
         "statlog_landsat_satellite": 146,
-        "mice_protein_expression": 342,
+        #"mice_protein_expression": 342, # missing values :/
         "diabetic_retinopathy_debrecen": 329,
         "isolet": 54,
         "chess_king_rook_vs_king_pawn": 22,
@@ -56,23 +55,28 @@ class Dataset:
 
         self.X = None
         self.y = None
+        self.metadata = None
         self.variables = None
 
         self.n_instances = None
         self.n_features = None
         self.n_targets = None
+        self.has_missing_values = None
 
         self.load()
 
     def load(self):
         dataset = fetch_ucirepo(id=self.id)
+
         self.X = dataset.data.features
         self.y = dataset.data.targets
+        self.metadata = dataset.metadata
         self.variables = dataset.variables
 
         self.n_instances = self.X.shape[0]
         self.n_features = len(self.variables[self.variables['role'] == 'Feature'])
         self.n_targets = len(self.variables[self.variables['role'] == 'Target'])
+        self.has_missing_values = self.metadata['has_missing_values']
 
         if False and self.n_targets == 0:
             print(dataset.data.targets, flush=True)
@@ -81,10 +85,11 @@ class Dataset:
         print(f"    Dataset {dataset.metadata['name']}:\n"
               f"     - {self.n_instances} instances\n"
               f"     - {self.n_features} features\n"
-              f"     - {self.n_targets} targets\n")
+              f"     - {self.n_targets} targets\n"
+              f"     - has missing values: {self.has_missing_values}\n")
 
     @classmethod
-    def load_n_datasets(cls, n=-1):
+    def load_datasets(cls, n=-1):
         dataset_ids = list(cls.DATASETS_IDS.items())[:n]
         datasets = []
         for i, (name, id) in enumerate(dataset_ids):
