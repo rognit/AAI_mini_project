7acf1f56797232e043f3669a85461df9f0fc0033
Émilie <emilie.duflos@insa-rouen.fr>
2025-01-09 21:42:25 +0100

    add svm and other changes

--- decision_tree.py
+++ decision_tree.py
@@ -1,11 +1,8 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.cluster import KMeans
-from sklearn.metrics import accuracy_score
-from scipy.optimize import linear_sum_assignment
 import numpy as np
-from sklearn.metrics import rand_score
-#from sklearn.utils import linear_assignment_
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 from dataset_config import DATASETS_IDS
 from Dataset import Dataset
@@ -16,19 +13,25 @@ def process_dataset(name, dataset_id):
         dataset = Dataset(id=dataset_id, name=name)
         X = dataset.X
         y = dataset.y
-        n_classes = len(np.unique(y))
+
+        # Allows you to have just one target, and for it to be a category.
         y = y.iloc[:, 0].astype('category').cat.codes
 
-        model = KMeans(n_clusters=n_classes, n_init='auto')
+        # Split data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        # Fit and predict the labels
-        y_pred = model.fit_predict(X)
+        # Train model
+        model = DecisionTreeClassifier(random_state=42)
+        model.fit(X_train, y_train)
 
-        score = rand_score(y, y_pred)
+        # Test model
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+
         return {
             "dataset_id": dataset_id,
             "name": name,
-            "rand_score": score
+            "accuracy": accuracy
         }
     except Exception as e:
         return {"dataset_id": dataset_id, "error": f"Processing error: {str(e)}"}
@@ -44,7 +47,7 @@ def main():
     results_df = pd.DataFrame(results)
     print("\n=== Final Result ===\n")
     print(results_df)
-    print(np.mean(results_df['rand_score']))
+    print(np.mean(results_df['accuracy']))
     return results_df
 
 if __name__ == "__main__":
